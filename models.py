1e759fb2e445e715dbc1eebd369cbfc2b748ddb9
Geena Jackson <geena.jackson96@gmail.com>
2022-10-04 09:59:40 -0500

    add models for teams, pokemon, moves, commment, ratings

--- models.py
+++ models.py
@@ -15,9 +15,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     email = db.Column(db.Text, nullable=False, unique=True)
-    
     username = db.Column(db.Text, nullable=False, unique=True)
-
     password = db.Column(db.Text, nullable=False)
 
     @classmethod
@@ -35,4 +33,86 @@ class User(db.Model):
     def authenticate(cls, username, password):
         """Authenticates a user with username and password."""
 
-        user = cls.query.filter_by(username=username)
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+        
+        return False
+
+class Team(db.Model):
+    """Model referring to a team of Pokemon."""
+
+    __tablename__ = "teams"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="cascade"))
+    details = db.Column(db.Text)
+
+class Pokemon(db.Model):
+    """Information about a specific Pokemon."""
+
+    __tablename__ = "pokemon"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    image = db.Column(db.Text, default="/static/images/poke-ball.png")
+    type_1 = db.Column(db.Text, nullable=False)
+    type_2 = db.Column(db.Text)
+
+class Move(db.Model):
+    """Information about a Pokemon Move."""
+
+    __tablename__ = "moves"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    # moves with no power are null
+    power = db.Column(db.Integer)
+    # moves that always hit are null
+    accuracy = db.Column(db.Integer)
+    pp = db.Column(db.Integer)
+
+class Moveset(db.Model):
+    """Connection of a Pokemon <-> a move."""
+
+    __tablename__ = "pokemon_moveset"
+
+    id = db.Column(db.Integer, primary_key=True)
+    poke_id = db.Column(db.Integer, db.ForeignKey("pokemon.id", ondelete="cascade"))
+    move_id = db.Column(db.Integer, db.ForeignKey("moves.id", ondelete="cascade"))
+
+class Team_Member(db.Model):
+    """Connection of a team <-> a Pokemon."""
+
+    __tablename__ = "team_members"
+
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id", ondelete="cascade"))
+    poke_id = db.Column(db.Integer, db.ForeignKey("pokemon.id", ondelete="cascade"))
+
+class Comment(db.Model):
+    """A comment on a team."""
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id", ondelete="cascade"))
+    comment = db.Column(db.Text, nullable=False)
+    commenter_id = db.Column(db.Text, db.ForeignKey("users.id", ondelete="cascade"))
+
+class Rating(db.Model):
+    """A rating on a team."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id", ondelete="cascade"))
+    rating = db.Column(db.Integer, nullable=False)
+    rater_id = db.Column(db.Text, db.ForeignKey("users.id", ondelete="cascade"))
+
+def connect_db(app):
+    """Connect this database to provided Flask app."""
+
+    db.app = app
+    db.init_app(app)

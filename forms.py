4d6969a55e334538c31fd2f7cd5eff5b452dd0bb
Geena Jackson <geena.jackson96@gmail.com>
2022-10-04 10:21:21 -0500

    add forms for user signup/login, comments

--- forms.py
+++ forms.py
@@ -1,3 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
+
+class UserSignUpForm(FlaskForm):
+    """Form for adding a new user."""
+
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[Length(min=8)])
+
+class LoginForm(FlaskForm):
+    """Form for logging in a user."""
+
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[Length(min=8)])
+
+class CommentForm(FlaskForm):
+    """Form for adding a comment."""
+
+    comment = TextAreaField("Comment", validators=[DataRequired()])

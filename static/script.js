2cc0485f94c73d8701e71824e65e1f96b776c937
Geena Jackson <geena.jackson96@gmail.com>
2022-10-06 10:49:42 -0500

    append API response to the DOM and display error message if pokemon is not found

--- static/script.js
+++ static/script.js
@@ -1,33 +1,44 @@
 const BASE_URL = "https://pokeapi.co/api/v2"
 const $pokemonSearch = $("#pokemon-search")
 const $term = $("#term")
-const $pokemonDisplay = $("pokemon-display")
+const $pokemonDisplay = $("#pokemon-display")
 
 async function getPokemon(term) {
     console.log("called")
-    const response = await axios({
-        url: `${BASE_URL}/pokemon/${term}`,
-        method: "GET"
-    });
-    const pokemon = response.data;
-    console.log("get pokemon:", pokemon)
-    return pokemon;
+    try {
+        const response = await axios({
+            url: `${BASE_URL}/pokemon/${term}`,
+            method: "GET"
+        });
+        const pokemon = response.data;
+        return pokemon;
+    }
+    catch (error) {
+        return undefined;
+    }
 }
 
-function showPokemon(pokemon) {
-    return $(`
-    <p>Name: ${pokemon.name}</p>
+function showPokemonMarkup(pokemon) {
+    if (pokemon == undefined) {
+        return $(`<p>Pokemon not found!</p>`)
+    }
+    else return $(`
+    <p><img src="${pokemon.sprites.front_default}">Name: ${pokemon.name}</p>
+    
     `)
+}
+
+async function getAndShowPokemon(term) {
+    const pokemon = await getPokemon(term)
+    const $pokemon = showPokemonMarkup(pokemon)
+    $pokemonDisplay.append($pokemon[0])
+    console.log($pokemon[0])
+    $pokemonDisplay.show()
 }
 
 $pokemonSearch.on("submit", function (evt) {
     evt.preventDefault();
-    const term = $term.val();
-    console.log("term:", term)
-    const pokemon = getPokemon(term);
-    console.log(pokemon)
-    const markup = showPokemon(pokemon)
-    $pokemonDisplay.append(markup)
-    $pokemonDisplay.show();
-    return false;
+    const term = $term.val().toLowerCase();
+    console.log("term:", term);
+    getAndShowPokemon(term);
 })
